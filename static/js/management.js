717b8eee3aea099f241d2292805902ab9925eede
hongtrangv <hongtrangv@gmail.com>
2025-12-09 17:49:00 +0000

    điều chỉnh icon svg

--- static/js/management.js
+++ static/js/management.js
@@ -151,8 +151,8 @@ function renderRecordsTable(item) {
                 <td class="py-2 px-4 text-center">${formatDate(record['date']) || 'N/A'}</td>`;
         }
         tableHtml += `<td class="py-3 px-4 flex items-center justify-end space-x-2">
-            <button class="p-1 hover:bg-gray-200 rounded-full edit-record-btn" data-type-id="${item.id}" data-record-id="${record.id}" title="Sửa"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-blue-600" viewBox="0 0 20 20" fill="currentColor"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" /><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" /></svg></button>
-            <button class="p-1 hover:bg-gray-200 rounded-full delete-record-btn" data-type-id="${item.id}" data-record-id="${record.id}" title="Xóa"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-red-600" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm4 0a1 1 0 012 0v6a1 1 0 11-2 0V8z" clip-rule="evenodd" /></svg></button>
+            <button class="p-1 hover:bg-gray-200 rounded-full edit-record-btn" data-type-id="${item.id}" data-record-id="${record.id}" title="Sửa"><svg class="h-5 w-5 text-blue-600"><use href="#icon-pencil"></use></svg></button>
+            <button class="p-1 hover:bg-gray-200 rounded-full delete-record-btn" data-type-id="${item.id}" data-record-id="${record.id}" title="Xóa"><svg class="h-5 w-5 text-red-600"><use href="#icon-trash"></use></svg></button>
             </td></tr>`;
     });
     tableHtml += '</tbody></table></div>';
@@ -228,9 +228,7 @@ async function loadMonthData(year, month, monthElement) {
         tabBarHtml += `
                     <div class="flex-shrink-0 p-2 border-r border-gray-200">
                         <button class="btn btn-primary" data-tab-id="${newTabId}">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2 -ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                            </svg>
+                            <svg class="w-5 h-5 mr-2 -ml-1"><use href="#icon-plus"></use></svg>
                             Thêm khoản mục
                         </button>
                     </div>
